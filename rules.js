cd2fed20addcd63e8e68f347c07dca09b106de7e
bosstypeshit <toltogazcrew@gmail.com>
2026-06-23 02:22:40 +0200

    Update rules.js

--- rules.js
+++ rules.js
@@ -27,14 +27,48 @@ const RULE_SECTIONS = [
     title: "Általános szabályok",
     tag: "Alap szabályzat",
     rules: [
-      "Ide írd az első általános szabályt.",
-      "Ide írd a második általános szabályt.",
-      "Ide írd a harmadik általános szabályt.",
-      "Ide írd a negyedik általános szabályt.",
-      "Ide írd az ötödik általános szabályt.",
-      "Ide írd a hatodik általános szabályt.",
-      "Ide írd a hetedik általános szabályt.",
-      "Ide írd a nyolcadik általános szabályt."
+      "TILOS a DM, más játékosok megtámadása / megölése előzmény nélkül",
+
+"TILOS az MG, olyan információk felhasználása amit az IC karaktered nem tudhat. (Pl. Stream-ben látod, hogy épp drogot csinálnak, te pedig odamész és lelövöd őket.)",
+
+"TILOS az SK, RK, NONRP, Force RP.",
+
+"Az NRK csak enyhített formában engedélyezett, viszont nagyobb ugratások, sportautókkal földúton száguldani stb. TILOS.",
+
+"A PG csak enyhített formában engedélyezett, tehát ha 2-en fognak rád fegyvert, az egyik meg leteszi / elnéz másfele, az nem minősül PG-nek, de ITT IS kötelező az életedet a legfontosabb tényezőként kezelni. Viszont ha pl. 5-en fognak rád fegyvert és tovább rádiózol, futkározol az már PG-nek minősül és TILOS.",
+
+"TILOS az Öngyilkos RP, Pl.: Lefegyverzés közben leugrasz a hegyről, ezzel elkerülve az RP-szálat.",
+
+"TILOS az IC-OOC keverése, mindent IC keretek között kell tartani.",
+
+"TILOS IC bármilyen szabályra utalni.",
+
+"TILOS a Soundboard használata.",
+
+"Mindig maradj karakterben még szabálytalan viselkedés esetén is.",
+
+"Az életed legyen az első minden esetben, ha fegyvert fognak rád annak megfelelően kell viselkedned is.",
+
+"TILOS az irreális viselkedés az NPC-kel, Pl: Indokolatlanul lelőni, elütni őket.",
+
+"Kötelező a Mexikói / Dél-Amerikai öltözetstílusnak, illetve a karakteredhez megfelelően öltözködni.",
+
+"TILOS a toxikus viselkedés minden formája.",
+
+"TILOS az RP Alóli kilépés Pl: A rendvédelem beraideli a HQ-d, te pedig kilépsz, hogy ne kapjanak el.",
+
+"TILOS bármilyen segédprogram használata, amik előnyhöz juttatnak más játékosokkal szemben. (Célkereszt IS!)",
+
+"TILOS a Magyar, illetve Fun nevek (Pl.: Obumbu Kumbula) használata IC.",
+
+"TILOS valós személyek nevét használni, kivéve a frakcióknak megfelelő vezetőjének nevét, Pl.: CJNG-nek El Mencho, stb.)",
+
+"TILOS bármilyen OOC név vagy becenév használata IC.",
+
+"A szerveren kötelező Push-To-Talk használata.",
+
+"TILOS a rádiót használni, ha a karaktered eszméletlen, bilincsben van, vagy elvették tőle."
+
     ]
   },
   {
